c5d3cb27eea955dcf4838335d13e9c3e2035ea5a
ChenKachlon <73073301+ChenKachlon@users.noreply.github.com>
2023-06-12 20:18:32 +0300

    dynamic graph

--- lib/apps/reports/report.dart
+++ lib/apps/reports/report.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
@@ -19,6 +20,9 @@ class ReportPage extends StatefulWidget {
 class _ReportPageState extends State<ReportPage> {
   EncryptData crypto = Get.find();
   
+  final _str = TextEditingController();
+  final List<String> dataoptions = ['gender', 'smoker'];
+
 
   Widget buildPatient(Patient patient) => RawChip(
         labelPadding: const EdgeInsets.all(2.0),
@@ -41,7 +45,7 @@ class _ReportPageState extends State<ReportPage> {
                   builder: (context) => PatientReportPage(patient: patient)));
         },
       );
-
+  
   @override
   Widget build(BuildContext context) {
 
@@ -54,8 +58,52 @@ class _ReportPageState extends State<ReportPage> {
 
 
 
+  Widget _checkBoxField({
+    required String title,
+    required TextEditingController controller,
+    required List<String> options,
+    }) {
+      return Row(
+      children: [
+        Expanded(
+          child: Text(
+            title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: ListTile(
+            title: Text(options[0]),
+            leading: Radio<String>(
+              value: options[0],
+              groupValue: controller.text,
+              onChanged: (String? value) {
+                setState(() {
+                  controller.text = value!;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListTile(
+            title: Text(options[1]),
+            leading: Radio<String>(
+              value: options[1],
+              groupValue: controller.text,
+              onChanged: (String? value) {
+                setState(() {
+                  controller.text = value!;
 
-
+                });
+              },
+            ),
+          ),
+        ),
+      ]
+    );
+  }
 
     return Scaffold(
               body: 
@@ -68,7 +116,12 @@ class _ReportPageState extends State<ReportPage> {
                       final patients = snapshot.data!;
                       return Column(
                         children: [
-                          NewWidget(),
+                          _checkBoxField(
+                            title: "Choose the data you want to display:",
+                            controller: _str,
+                            options: dataoptions,
+                          ),
+                          newW(),
                           Padding(
                             padding: const EdgeInsets.all(18.0),
                             child: Column(
@@ -77,7 +130,6 @@ class _ReportPageState extends State<ReportPage> {
                                   child: Text(
                                     "Calculate the recommendation for the cost of annual dental insurance for patient ",
                                     style: TextStyle(fontSize: 30, color: Colors.black)
-                                    
                                     ),
                                 ),
                                 const SizedBox(height: 10),
@@ -101,51 +153,51 @@ class _ReportPageState extends State<ReportPage> {
                 )
             ,);
   }
-}
 
-class NewWidget extends StatelessWidget {
-  NewWidget({super.key});
-
-
-
-  Stream<List<Treatment>> readTreatments() => FirebaseFirestore.instance
-      .collection('Treatments')
-      .snapshots()
-      .map((snapshot) => snapshot.docs
-          .map((doc) => Treatment.fromJson(doc.data()))
-          .toList());
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<List<Treatment>>(
-      stream: readTreatments(),
+  Widget newW (){
+    return StreamBuilder<List<Patient>>(
+      stream: readPatients(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return Text('something went wrong ${snapshot.error}');
         } else if (snapshot.hasData) {
-          final treatments = snapshot.data!;
-          final groupedTreatments = groupBy(treatments, (treatment) => treatment.treatingDoctor);
-          final data = groupedTreatments.entries.map((entry) => _SalesData(entry.key, entry.value.fold(0, (sum, treatment) => sum + treatment.cost))).toList();
-          return Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: SfCartesianChart(
-              primaryXAxis: CategoryAxis(),
-              // Chart title
-              title: ChartTitle(text: 'Sales by doctor analysis'),
-              // Enable legend
-              legend: Legend(isVisible: true),
-              // Enable tooltip
-              tooltipBehavior: TooltipBehavior(enable: true),
-              series: <ChartSeries<_SalesData, String>>[
+          final patients = snapshot.data!;
+          // ignore: avoid_init_to_null
+          var x = null;
+          if (_str.text == "gender") {
+            final groupedPatients = groupBy(patients, (patient) => crypto.decryptAES(patient.gender));
+            x = groupedPatients.entries.map((entry) => _SalesData(entry.key, entry.value.fold(0, (previousValue, element) => previousValue + 1))).toList();
+          }
+          else{
+            final groupedPatients = groupBy(patients, (patient) => crypto.decryptAES(patient.smoker));
+            x = groupedPatients.entries.map((entry) => _SalesData(entry.key, entry.value.fold(0, (previousValue, element) => previousValue + 1))).toList();
+          }
+          final data = x!;
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: 
+                    SfCartesianChart(
+                      primaryXAxis: CategoryAxis(),
+                      // Chart title
+                      title: ChartTitle(text: 'Patients ${_str.text} Analysis'),
+                      // Enable legend
+                      legend: Legend(isVisible: true),
+                      // Enable tooltip
+                      tooltipBehavior: TooltipBehavior(enable: true),
+                      series: <ChartSeries<_SalesData, String>>[
 
-                LineSeries<_SalesData, String>(
-                    dataSource: data,
-                    xValueMapper: (_SalesData sales, _) => sales.doctor,
-                    yValueMapper: (_SalesData sales, _) => sales.sales,
-                    name: 'Sales',
-                    // Enable data label
-                    dataLabelSettings: DataLabelSettings(isVisible: true))
-              ]),
+                        ColumnSeries<_SalesData, String>( //colmn sres todo 
+                            dataSource: data,
+                            xValueMapper: (_SalesData sales, _) => sales.x,
+                            yValueMapper: (_SalesData sales, _) => sales.y,
+                            name: _str.text,
+                            // Enable data label
+                            dataLabelSettings: const DataLabelSettings(isVisible: true))
+                      ]),
+              ),
+            ],
           );
         } else {
           return const CircularProgressIndicator(
@@ -158,10 +210,10 @@ class NewWidget extends StatelessWidget {
   }
 }
 
-class _SalesData {
-  _SalesData(this.doctor, this.sales);
 
-  final String doctor;
-  final double sales;
+class _SalesData {
+  _SalesData(this.x, this.y);
+  final String x;
+  final double y;
 }
 
